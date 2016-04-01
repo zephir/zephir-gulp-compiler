de14e186ecebd9fdbe29b05423aaaede6e21def3
Marc Stampfli <marc.stampfli@zephir.ch>
2016-04-01 11:03:42 +0200

    Update

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 'use strict';
 
-var gulp = require('gulp');
+module.exports = function (gulp) {
 
-module.exports = function () {
+    // Try to determine enviroment. If nothing's given, use "local".
+    var argv = require('yargs').argv;
+
+    var env = "local";
+    if (argv.dev == true) {
+        env = "dev";
+    } else if (argv.prep == true || argv.preproduction == true) {
+        env = "prep";
+    } else if (argv.prod == true || argv.production == true) {
+        env = "prod";
+    }
 
     gulp.task('default', function() {
-        console.log("SUCCESS!");
+        console.log(env);
     });
-    
+
 };
