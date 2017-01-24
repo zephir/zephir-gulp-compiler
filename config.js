69af0557ac3ee7a8c8b0c798bfb55fb49655b7b4
Basil <git@nadar.io>
2017-01-24 13:39:27 +0100

    Listen to HTML (#6)
    Not sure this will work, but it should also listen to html files when drafting a new website with an index.html file?

--- config.js
+++ config.js
@@ -10,7 +10,8 @@ module.exports = {
 
     "source": {
         "filesToWatch": [
-            "../**/*.php"
+            "../**/*.php",
+            "../**/*.html"
         ],
         "styles": [
             "scss/**/*.scss"
